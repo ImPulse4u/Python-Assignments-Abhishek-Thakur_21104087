8febeabcc35827238e0ffba692a73b408c4fb281
bt21104087abhi <97660883+bt21104087abhi@users.noreply.github.com>
2022-01-25 18:55:01 +0530

    Add files via upload

---
+++ Assignment 2 output.py
@@ -0,0 +1,39 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+= RESTART: C:\Users\Abhishek Thakur\Desktop\Python Assignments\A2\Assignment 2.py
+35
+egaugnal evitisnes esac a si nohtyP
+a case sensitive
+Python is object oriented language
+10
+Pythonisacasesensitivelanguage
+
+Hey,Abhishek Thakur Here!
+My SID is 21104087
+I am from EE department and my CGPA is 9.9
+
+0b1000
+
+0b111010
+
+0b110010
+
+0b11100000
+0b101000
+
+0b1110
+0b0
+
+Give first number:345
+Give second number:567
+Give third number:237
+567 is greatest number
+
+Enter string:My college is located in Chandigarh
+No
+
+Enter length of first side:3
+Enter length of second side:4
+Enter length of third side:5
+Yes
